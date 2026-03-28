cd1ca2603429c3568977c09c0300a3878cc248fe
Valerie Bolden <valerie.bolden@gmail.com>
2026-03-28 16:50:18 -0400

    made addItem function to add blog post to aside section and made a clearInput function to clear input fields after post is added

--- script.js
+++ script.js
@@ -2,10 +2,46 @@
 let postTitle = document.getElementById("post-title");
 let postText = document.getElementById("post-text");
 let addPostBtn = document.getElementById("addButton");
+let blogCards = document.getElementById('cards-container');
 
 
 // EVENT LISTENERS
 addPostBtn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log("Hello there");
+
+    addItem();
+    clearInput();
 });
+
+
+// FUCNTIONS
+function addItem() {
+    let postCard = document.createElement('div');
+    postCard.classList.add("card-body")
+
+    let postCardTitle = document.createElement('h4');
+    postCardTitle.innerHTML = postTitle.value;
+
+    let postCardBody = document.createElement('p');
+    postCardBody.innerHTML = postText.value;
+
+    let editPost = document.createElement('button');
+    editPost.id = "edit"
+    editPost.innerHTML = "Edit Post"
+
+    let removePost = document.createElement('button');
+    removePost.id = "remove"
+    removePost.innerHTML = "Remove Post"
+
+    postCard.appendChild(postCardTitle);
+    postCard.appendChild(postCardBody);
+    postCard.appendChild(editPost);
+    postCard.appendChild(removePost);
+    blogCards.appendChild(postCard);
+}
+
+function clearInput() {
+    postTitle.value = '';
+    postText.value =  '';
+}
