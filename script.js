8847338b9a991bae4c8671c7a9e86709cd867a00
Valerie Bolden <valerie.bolden@gmail.com>
2026-03-28 15:30:15 -0400

    added cached variables and button event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+// VALUES
+let postTitle = document.getElementById("post-title");
+let postText = document.getElementById("post-text");
+let addPostBtn = document.getElementById("addButton");
+
+
+// EVENT LISTENERS
+addPostBtn.addEventListener('click', (e) => {
+    console.log("Hello there")
+})
